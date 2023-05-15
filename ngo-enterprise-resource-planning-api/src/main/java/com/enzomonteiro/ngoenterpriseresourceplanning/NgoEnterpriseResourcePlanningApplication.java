cc2a161c7c35b826cc8c470911235ab7590f832e
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-05-15 16:02:01 -0300

    Creating API Project

---
+++ ngo-enterprise-resource-planning-api/src/main/java/com/enzomonteiro/ngoenterpriseresourceplanning/NgoEnterpriseResourcePlanningApplication.java
@@ -0,0 +1,13 @@
+package com.enzomonteiro.ngoenterpriseresourceplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoEnterpriseResourcePlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoEnterpriseResourcePlanningApplication.class, args);
+	}
+
+}
---
+++ ngo-enterprise-resource-planning-api/src/test/java/com/enzomonteiro/ngoenterpriseresourceplanning/NgoEnterpriseResourcePlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzomonteiro.ngoenterpriseresourceplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoEnterpriseResourcePlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
