0d085054a00f5e6197d178e69a8d026de9c7afe4
Enzo Vila Nova Monteiro <enzovila.monteiro@gmail.com>
2023-05-16 01:36:59 -0300

    Creating sql for tests and adding Ngo Controller

---
+++ ngo-enterprise-resource-planning-api/src/main/java/com/enzomonteiro/ngoenterpriseresourceplanning/controllers/NgoController.java
@@ -0,0 +1,21 @@
+package com.enzomonteiro.ngoenterpriseresourceplanning.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.enzomonteiro.ngoenterpriseresourceplanning.dto.NgoDTO;
+import com.enzomonteiro.ngoenterpriseresourceplanning.services.NgoService;
+
+@RestController
+@RequestMapping("/ngos")
+public class NgoController {
+	
+	@Autowired
+	private NgoService ngoService; 
+	
+	public List<NgoDTO> findAll(){
+		return ngoService.findAll();
+	}
+}
